d97a9d46fb43d41d268388b632b3811e65722add
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-23 00:33:22 +0200

    Avoid using asterisks in imports.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
-from .stock import *
+from . import product
+from . import stock
 
 
 def register():
     Pool.register(
-        Template,
-        Configuration,
-        ConfigurationProductTemplateFormQuantity,
+        product.Template,
+        stock.Configuration,
+        stock.ConfigurationProductTemplateFormQuantity,
         module='product_template_form_quantity', type_='model')
